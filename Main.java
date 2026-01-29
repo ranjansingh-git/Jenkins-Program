acf4b13d0bd5e2e8a34371dbc3bb61cb834f46d1
ranjansingh-git <amranjansingh@gmail.com>
2026-01-29 18:45:18 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+import java.util.Date;
+public class Main {
+
+		public static void main(String args[]) {
+			System.out.println("This is Java program");
+			System.out.println("Output from Java Program");
+			System.out.println(new Date());
+			System.out.println("Hello 123");
+		}
+}
